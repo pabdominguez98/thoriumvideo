80db48c0cc1e35b05d28b2c08e8d9d1288fb75cf
Pablo Dominguez <dominguezpablo0998@gmail.com>
2020-10-08 17:13:28 -0300

    Carousel Act

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import Header from '../components/Header';
+import '../assets/styles/App.scss';
+import Search from '../components/Search';
+import Categories from '../components/Categories';
+import Carousel from '../components/Carousel';
+import Carouselitem from '../components/Carouselitem';
 
 const App = () => (
    <div className="App">
       <Header />
+      <Search />
+      <Categories>
+         <Carousel>
+            <Carouselitem />
+            <Carouselitem />
+            <Carouselitem />
+            <Carouselitem />
+         </Carousel>
+      </Categories>
    </div>
 );
 
