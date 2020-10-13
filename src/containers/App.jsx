a2df7b1c01f43de4ccc401220b993257d5a84df5
Pablo Dominguez <dominguezpablo0998@gmail.com>
2020-10-13 18:17:42 -0300

    Actualizacion 12/10/2020

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import '../assets/styles/App.scss';
 import Search from '../components/Search';
@@ -8,48 +8,41 @@ import Carouselitem from '../components/Carouselitem';
 import Footer from '../components/Footer';
 
 
-const App = () => (
-   <div className="App">
-      <Header />
-      <Search />
-      <Categories title="Mi lista">
-         <Carousel>
-            <Carouselitem />
-            <Carouselitem />
-            <Carouselitem />
-            <Carouselitem />
-            <Carouselitem />
-            <Carouselitem />
-            <Carouselitem />
-            <Carouselitem />
-         </Carousel>
-      </Categories>
-      <Categories title="Tendencias">
-         <Carousel>
-            <Carouselitem />
-            <Carouselitem />
-            <Carouselitem />
-            <Carouselitem />
-            <Carouselitem />
-            <Carouselitem />
-            <Carouselitem />
-            <Carouselitem />
-         </Carousel>
-      </Categories>
-      <Categories title="Mas buscados">
-         <Carousel>
-            <Carouselitem />
-            <Carouselitem />
-            <Carouselitem />
-            <Carouselitem />
-            <Carouselitem />
-            <Carouselitem />
-            <Carouselitem />
-            <Carouselitem />
-         </Carousel>
-      </Categories>
-      <Footer />
-   </div>
-);
+const App = () => {
+   const [videos, setVideos] = useState({ mylist: [], trends: [], originals: [] });
+
+   useEffect(() => {
+      fetch('http://localhost:3000/initalState') // aca va la direccion de la api que nos queremos comunicar
+         .then(res => res.json())
+         .then(data => setVideos(data));
+   }, []);
+
+   return (
+      <div className="App">
+         <Header />
+         <Search />
+         {videos.mylist.length > 0 &&
+            <Categories title="Mi lista">
+               <Carousel>
+                  <Carouselitem />
+               </Carousel>
+            </Categories>
+         }
+         <Categories title="Tendencias">
+            <Carousel>
+               {videos.trends.map(item =>
+                  <Carouselitem key={item.id} {...item}/>
+               )}
+            </Carousel>
+         </Categories>
+         <Categories title="Mas buscados">
+            <Carousel>
+               <Carouselitem />
+            </Carousel>
+         </Categories>
+         <Footer />
+      </div>
+   )
+};
 
 export default App;
