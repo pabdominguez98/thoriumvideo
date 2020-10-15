b13881fb16bb251edf088a021ce14ccdd17e657f
Pablo Dominguez <dominguezpablo0998@gmail.com>
2020-10-15 02:06:05 -0300

    Validacion de datos del Login

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import googleIcon from '../assets/static/google-icon.png';
 import twitterIcon from '../assets/static/twitter-icon.png';
 import '../assets/styles/components/Login.scss';
 import { Link } from 'react-router-dom';
 
-const Login = () => (
+const Login = () =>{
+const [form, setValues] = useState({
+   email:'',
+
+});
+ 
+const handleInput = event => {
+   setValues({
+     ...form,
+     [event.target.name]: event.target.value
+   });
+}
+
+const handleSubmit = event => {
+    event.preventDefault();
+    console.log(form);
+}
+
+return (
 
   <section className="login">
     <section className="login__container">
       <h2>Inicia sesión</h2>
-      <form className="login__container--form">
-        <input className="input" type="text" placeholder="Correo" />
-        <input className="input" type="password" placeholder="Contraseña" />
+      <form className="login__container--form" onSubmit={handleSubmit}>
+        <input
+         name="email"
+         className="input"
+         type="text" 
+         placeholder="Correo" 
+         onChange={handleInput}
+        />
+        <input 
+         name="password"
+         className="input" 
+         type="password" 
+         placeholder="Contraseña" 
+         onChange={handleInput}
+         />
         <button className="button">Iniciar sesión</button>
         <div className="login__container--remember-me">
           <label>
@@ -33,5 +63,6 @@ const Login = () => (
   </section>
 
 );
+}
 
 export default Login;
